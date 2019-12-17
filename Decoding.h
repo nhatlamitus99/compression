1c71f7ae85eff6ef6cbb808a5f0d01bf29720da7
nhatlamitus99 <58985591+nhatlamitus99@users.noreply.github.com>
2019-12-17 22:25:09 +0700

    Add files via upload
    code to decompress files

---
+++ Decoding.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<stdio.h>
+
+void LoadHuffmanTree(FILE *&fpread);
+int DemSoKiTu();
+int getbit(unsigned char n, int i);
+void onbit(char unsigned &n, int i);
+void GiaiNen(int nRoot, FILE *& fpread);
